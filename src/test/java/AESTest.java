10650179ed5de130ef1d18d35bbac6d506fc2a8a
Dominik Kania <dominik.kania100@gmail.com>
2022-08-03 16:23:38 +0200

    feat: add dummy test

---
+++ src/test/java/AESTest.java
@@ -0,0 +1,10 @@
+import static org.junit.Assert.assertEquals;
+
+public class AESTest {
+    @org.junit.Test
+    public void shouldBeTrue(){
+        int a = 10;
+        int b = 20;
+        assertEquals(200, a*b);
+    }
+}
