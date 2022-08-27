d1e87b5a70d99c0c64ad08883bee239b6dc07c71
Dominik Kania <dominik.kania100@gmail.com>
2022-08-27 13:49:26 +0200

    test: add Simon SAEB test

--- src/test/java/SAEBTest.java
+++ src/test/java/SAEBTest.java
@@ -1,27 +1,26 @@
 import ciphers.AES;
-import org.junit.Assert;
+import ciphers.SimonEngine;
 import org.junit.Test;
 import saeb.SAEB;
 import saeb.SAEBResult;
-
-import java.util.Arrays;
 
 import static org.junit.Assert.assertArrayEquals;
 
 public class SAEBTest {
 
+    byte[] plaintext = "randomStringPlaintext".getBytes();
+    byte[] iv = "0000000000100000".getBytes();
+    byte[] associatedData = "RandomAssociatedDataString".getBytes();
+    byte[] nonce = "15".getBytes();
+    String KEY = "000102030405060708090a0b0c0d0e0f";
+
     @Test
-    public void saebTestWithParameters(){
+    public void saebTestWithAes(){
         int n = 16;
         int r = 6;
         int r1 = 10;
         int t = 5;
 
-        byte[] plaintext = "randomStringPlaintext".getBytes();
-        byte[] iv = "0000000000100000".getBytes();
-        byte[] associatedData = "RandomAssociatedDataString".getBytes();
-        byte[] nonce = "15".getBytes();
-        String KEY = "000102030405060708090a0b0c0d0e0f";
         AES aes = new AES(KEY.getBytes());
 
         SAEB saeb = new SAEB(n, r1, r, t, aes);
@@ -34,4 +33,21 @@ public class SAEBTest {
         assertArrayEquals(plaintext, decryptedPlaintext);
     }
 
+    @Test
+    public void saebTestWithSimon(){
+        int n = 16;
+        int r = 6;
+        int r1 = 10;
+        int t = 5;
+
+        SimonEngine simon = new SimonEngine(128, KEY.getBytes());
+
+        SAEB saeb = new SAEB(n, r1, r, t, simon);
+
+        SAEBResult saebEncResult = saeb.encrypt(nonce, associatedData,plaintext);
+
+        byte[] decryptedPlaintext = saeb.decrypt(nonce, associatedData,saebEncResult.getResult(), saebEncResult.getTag());
+
+        assertArrayEquals(plaintext, decryptedPlaintext);
+    }
 }
